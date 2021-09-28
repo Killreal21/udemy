0f4b665428bf48eafa8299e0f8821930d37bdb1c
kirill <chernega.kirill@mail.ru>
2021-09-28 18:46:20 +0300

    first commit

---
+++ js/learning.js
@@ -0,0 +1,60 @@
+"use strict";
+
+//объекты
+// const person = {
+//     firstName: 'Kirya',
+//     lastName: 'Ch',
+//     year: 1993,
+// };
+
+// console.log(1);
+
+// let number = 5;
+
+// const $dsfg = "$dsfg";
+
+// console.log($dsfg);
+
+// const obj = {
+//     name: 'John',
+//     isMarried: false,
+//     age: 25,
+// }
+
+// console.log(obj.name);
+
+// let arr = ['sho.png', 'nisho.org', 'ok'];
+
+// console.log(arr[1]);
+
+// const res = confirm('Как ты?');
+
+// console.log(res);
+
+// const answer = +prompt('Вам есть 18?', "18");
+// console.log(answer+5);
+
+// const answers = []; //объявление массива
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Какая у вас фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+
+// const cat = 'categ';
+
+// console.log(`https://sait.ru/${cat}/5`);
+
+// const user ='Kirya';
+
+// alert(`Привет, ${user}`);
+
+// let incr =10, decr = 10;
+
+// ++incr;
+// --decr;
+
+// console.log(incr, decr);
+
+const isChecked = false, isClose = false;
